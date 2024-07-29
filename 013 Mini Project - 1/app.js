4acf3447ad5e5833726787f1508dd6a826a02e8d
Ishak Shekh <shekhishak90@gmail.com>
2024-07-30 00:16:56 +0530

    Intializes Multer For File Upload

--- 013 Mini Project - 1/app.js
+++ 013 Mini Project - 1/app.js
@@ -4,9 +4,11 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userModel = require("./models/user.model");
 const postModel = require("./models/post.model");
+const multer = require("multer");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname + "public")));
@@ -14,8 +16,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
+// Multer Disk Storage
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/uploads");
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12, (err, bytes) => {
+      const fn = bytes.toString("hex") + path.extname(file.originalname);
+      cb(null, fn);
+    });
+  },
+});
+
+const upload = multer({ storage: storage });
+//
+
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+// file upload
+app.get("/test", (req, res) => {
+  res.render("test");
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  // console.log(req.body);
+  console.log(req.file);
 });
 
 // User Login Logic
